0903b82b9cf37ed3c52b84ba949449f3cccab0ba
akashm1110 <akashsanjay1626@gmail.com>
2024-08-18 02:22:01 +0530

    Initial code commit

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,31 @@
+const OrderModel = require('../models/orderModel');
+const ProductModel = require('../models/productModel');
+
+
+//create order - /api/v1/order
+exports .createOrder = async(req,res,next) =>{
+
+   const cartItems = req.body;
+   const amount = Number(cartItems.reduce((acc,item) =>(acc + item.product.price * item.qty),0)).toFixed(2);
+    const status = 'pending';
+
+    const order =await OrderModel.create({cartItems,amount,status});
+
+    
+
+    //updating product stocks
+
+    
+
+    cartItems.forEach(async (item)=> {
+        const product = await ProductModel.findById(item.product._id);
+        product.stock = product.stock - item.qty;
+        await product.save();
+    })
+
+    res.json({
+        success : true,
+        order
+    })
+    }
+
